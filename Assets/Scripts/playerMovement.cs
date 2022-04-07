d53404428385647370dbc04445293fefe88b2002
Ye Fang "Norman" Kuang <normankuang@msn.com>
2022-04-06 20:57:20 -0400

    add player, walls and goal

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        Vector2 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("GameObject1 collided with " + col.name);
+    }
+}
